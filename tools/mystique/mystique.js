7867de994fad426059efcdee80d4fd38786e9f56
Vitaly Tsaplin <tsaplin@adobe.com>
2024-06-04 13:52:57 +0200

    feat: update plugin

--- tools/mystique/mystique.js
+++ tools/mystique/mystique.js
@@ -1,18 +1,5 @@
-function loadMystique() {
-  const url = 'http://localhost:8080/index.js';
-  const script = document.createElement('script');
-  script.setAttribute('src', url);
-  script.setAttribute('type', 'text/javascript');
-  script.setAttribute('async', 'true');
-  script.crossOrigin = 'anonymous';
-  document.head.appendChild(script);
-}
-
 if (window.location.hostname === ('localhost')) {
   const searchParams = new URLSearchParams(window.location.search);
-  if (searchParams.has('mystique')) {
-    loadMystique();
-  }
   if (searchParams.has('toggleStyle')) {
     const styleName = searchParams.get('toggleStyle');
     // eslint-disable-next-line no-console
