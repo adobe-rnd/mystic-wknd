81dfdbf73833592881e7a9da10826c4e307ac809
Vitaly Tsaplin <tsaplin@adobe.com>
2024-06-18 14:04:48 +0200

    feat: add genvar poc v2 plugin

--- tools/genvar/genvar.js
+++ tools/genvar/genvar.js
@@ -1,20 +1,22 @@
-const GENVAR_EVENT = 'genvar';
+(() => {
+  const GENVAR_EVENT = 'genvar-poc';
 
-const genvar = async ({ detail }) => {
-  // const sk = detail.data;
-  const script = document.createElement('script');
-  script.src = 'http://localhost:4081/';
-  script.type = 'text/javascript';
-  script.async = true;
-  document.head.appendChild(script);
-};
+  const genvar = async ({ detail }) => {
+    // const sk = detail.data;
+    const script = document.createElement('script');
+    script.src = 'http://localhost:4081/';
+    script.type = 'text/javascript';
+    script.async = true;
+    document.head.appendChild(script);
+  };
 
-const sk = document.querySelector('helix-sidekick');
-if (sk) {
-  sk.addEventListener(`custom:${GENVAR_EVENT}`, genvar);
-} else {
-  document.addEventListener('sidekick-ready', () => {
-    document.querySelector('helix-sidekick')
-      .addEventListener(`custom:${GENVAR_EVENT}`, genvar);
-  }, { once: true });
-}
+  const sk = document.querySelector('helix-sidekick');
+  if (sk) {
+    sk.addEventListener(`custom:${GENVAR_EVENT}`, genvar);
+  } else {
+    document.addEventListener('sidekick-ready', () => {
+      document.querySelector('helix-sidekick')
+        .addEventListener(`custom:${GENVAR_EVENT}`, genvar);
+    }, { once: true });
+  }
+})();
--- tools/mystique/mystique.js
+++ tools/mystique/mystique.js
@@ -1,20 +1,22 @@
-const MYSTIQUE_PLAYGROUND_EVENT = 'mystique-playground';
+(() => {
+  const EVENT_NAME = 'mystique-playground';
 
-const mystique = async ({ detail }) => {
-  // const sk = detail.data;
-  const script = document.createElement('script');
-  script.src = 'http://localhost:4010/assistant.js';
-  script.type = 'text/javascript';
-  script.async = true;
-  document.head.appendChild(script);
-};
+  const mystique = async ({ detail }) => {
+    // const sk = detail.data;
+    const script = document.createElement('script');
+    script.src = 'http://localhost:4010/assistant.js';
+    script.type = 'text/javascript';
+    script.async = true;
+    document.head.appendChild(script);
+  };
 
-const sk = document.querySelector('helix-sidekick');
-if (sk) {
-  sk.addEventListener(`custom:${MYSTIQUE_PLAYGROUND_EVENT}`, mystique);
-} else {
-  document.addEventListener('sidekick-ready', () => {
-    document.querySelector('helix-sidekick')
-      .addEventListener(`custom:${MYSTIQUE_PLAYGROUND_EVENT}`, mystique);
-  }, { once: true });
-}
+  const sk = document.querySelector('helix-sidekick');
+  if (sk) {
+    sk.addEventListener(`custom:${EVENT_NAME}`, mystique);
+  } else {
+    document.addEventListener('sidekick-ready', () => {
+      document.querySelector('helix-sidekick')
+        .addEventListener(`custom:${EVENT_NAME}`, mystique);
+    }, { once: true });
+  }
+})();
